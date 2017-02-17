775ba47d8585db37dfcfe228cea248f33f814f04
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-02-17 18:40:49 +0200

    Add mesos-dns

--- scripts/broadcast-all-docker-images.sh
+++ scripts/broadcast-all-docker-images.sh
@@ -7,8 +7,10 @@ if [ -z $INVENTORY ]; then
 fi
 curl -s https://raw.githubusercontent.com/j3k0/mesos-compose/master/compose | grep '^IMAGE_' > .mesos-images
 . .mesos-images
+rm -f .mesos-images
 
 scripts/broadcast-docker-image.sh "$INVENTORY" jenserat/tinc
 scripts/broadcast-docker-image.sh "$INVENTORY" "$IMAGE_ZOOKEEPER"
 scripts/broadcast-docker-image.sh "$INVENTORY" "$IMAGE_MESOS"
 scripts/broadcast-docker-image.sh "$INVENTORY" "$IMAGE_MARATHON"
+scripts/broadcast-docker-image.sh "$INVENTORY" mesosphere/marathon-lb:v1.5.0
